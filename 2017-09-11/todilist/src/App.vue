537910bf5a34c85ce7d4a06a5629c374876e46ef
chenyang222 <351424947@qq.com>
2017-09-11 16:40:48 +0800

    20170911

---
+++ 2017-09-11/todilist/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    </h1>{{ title }}</h1>
+    <input v-model="newitem" @keyup.enter="addNew">
+    <ul v-for="item in items">
+      <li v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">
+        {{ item.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data: function(){
+        return{
+            title:"this is ranchy's todo list",
+            items:[
+    
+            ],
+            newitem:""
+        }
+    },
+    methods:{
+        toggleFinish:function(item){
+
+            item.isFinished = !item.isFinished
+
+        },
+        addNew:function(){
+            this.items.push({
+                label:this.newitem,
+                isFinished:false
+            })
+            this.newitem = "";
+        
+        }
+
+    }
+
+}
+
+</script>
+
+
+<style>
+.finished{
+  text-decoration:underline
+}
+
+</style>
