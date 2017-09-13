e4181895dfe308f3cbcc47f23fbccb08ed39b0ce
chenyang222 <351424947@qq.com>
2017-09-13 12:10:22 +0800

    20170913

---
+++ wynews/wynews/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+  	
+  	<view-box>
+  		<x-header></x-header>
+		  	
+  	</view-box>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+import {ViewBox , XHeader} from 'vux';
+
+export default {
+  name: 'app',
+  components:{
+  	ViewBox,
+  	XHeader
+  }
+}
+</script>
+
+<style lang="less">
+	
+	@import '~vux/src/styles/reset.less';
+
+	html,body{
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		height: 100%;
+		overflow-x: hidden;
+	}
+	#app{
+		height: 100%;
+	}
+
+</style>
